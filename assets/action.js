caac3d077c92e1b1a18e4f2c0f63bda03b4a25e6
Martin Winter <m.winter@jugendpresse.de>
2019-03-20 01:05:15 +0000

    prepare for generating noise on fuzzy tv screen

--- assets/action.js
+++ assets/action.js
@@ -1,5 +1,23 @@
 var filterwarnVisible = true;
 
+// create web audio api context
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+function playNote(frequency, duration) {
+	// create Oscillator node
+	var oscillator = audioCtx.createOscillator();
+
+	oscillator.type = 'square';
+	oscillator.frequency.value = frequency; // value in hertz
+	oscillator.connect(audioCtx.destination);
+	oscillator.start();
+
+	setTimeout(
+		function(){
+			oscillator.stop();
+			playMelody();
+		}, duration);
+}
+
 $(document).ready(function() {
     // adjust font size for whole page
     var size = parseFloat( $("body").css('font-size'), 10);
